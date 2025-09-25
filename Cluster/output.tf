455ec8206550814cf2a2f52860e912ac62671d93
Jasmine Shaik <sk.jasminesaheb@gmail.com>
2025-09-25 08:07:53 +0530

    Adding ecr eks vpc  providers files for terraform

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -1,16 +1,25 @@
-output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
-}
-
-output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
-}
-
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
 }
 
-output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+output "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = aws_eks_cluster.cluster.name
 }
 
+output "eks_cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.cluster.endpoint
+}
+
+output "ecr_repository_urls" {
+  description = "URLs of the ECR repositories"
+  value       = { for k, v in aws_ecr_repository.repo : k => v.repository_url }
+}
+
+# Command to update kubeconfig for the created cluster
+output "configure_kubectl" {
+  description = "Configure kubectl to use the new EKS cluster"
+  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.cluster.name}"
+}
--- Cluster/variables.tf
+++ Cluster/variables.tf
@@ -1,6 +1,43 @@
-variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for instances"
+
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
   type        = string
-  default     = "DevOps-Shack"
+  default     = "ap-south-1"
 }
 
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "jasmine-eks-cluster"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "azs" {
+  description = "Availability Zones"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
+
+variable "ecr_repo_names" {
+  description = "List of ECR repository names to create"
+  type        = list(string)
+  default     = ["jasmine-app-repo"]
+}
